4771245d17f346e151fe4c2b524b8988136d37f0
Pradeepkarki112 <pradeepkarki112@gmail.com>
2023-10-06 13:08:45 +0545

    all set to go

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from '../Extra/Footer';
+import Navbar from './../Extra/Navbar';
+
+export default function Home() {
+  return (
+    <div>
+        <Navbar />
+        <Footer />
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#45B8EA",
+      },
+
+      screens: {
+        // sm: "640px",
+        "2xl": "1280px",
+      },
+
+      fontSize: {
+        normalSize: "16px",
+        mediumSize: "24px",
+        largeSize: "40px",
+      },
+
+      letterSpacing: {
+        tight: "-.025em",
+        normal: "0",
+        wide: ".025em",
+      },
+    },
   },
   plugins: [],
-}
+};
