65874eaceeff4d8a521b893b7ac1b4a59c7394a6
Pradeepkarki112 <pradeepkarki112@gmail.com>
2023-10-08 15:57:19 +0545

    done

--- src/components/Pages/PrivacyPolicy.js
+++ src/components/Pages/PrivacyPolicy.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Navbar from "../Extra/Navbar";
+import Discover from "./../Extra/Discover";
+import Download from "./../Extra/Download";
+import Footer from "../Extra/Footer";
 
 export default function PrivacyPolicy() {
   return (
     <div>
       <Navbar />
-      <div>PrivacyPolicy</div>
+      <div>Privacy Policy for Medhavhi</div>
+      <div className="bg-backgroundColor">
+        <Discover />
+        <Download />
+        <Footer />
+      </div>
     </div>
   );
 }
